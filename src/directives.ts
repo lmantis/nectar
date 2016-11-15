ec9dda14da43675086ae28f44302fcb9085c26b8
Leo Mantis <leo.mantis@brafton.com>
2016-11-15 11:17:23 +0000

    backup before publishing

--- src/directives.ts
+++ src/directives.ts
@@ -1,13 +1,16 @@
 // Import all directives
 import {SampleDirective} from './directives/sample.directive';
 import {SampleComponent} from './directives/sample.component';
+import {HelloWorld} from './directives/hello-world.component';
 
 // Export all directives
 export * from './directives/sample.directive';
 export * from './directives/sample.component';
+export * from './directives/hello-world.component';
 
 // Export convenience property
 export const DIRECTIVES: any[] = [
   SampleDirective,
-  SampleComponent
+  SampleComponent,
+  HelloWorld
 ];
